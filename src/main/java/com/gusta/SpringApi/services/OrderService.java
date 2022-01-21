1f9a56f7e263fc2d291755cdd647d83c1ece3afb
Gustavo de Azevedo Pereira <gustavo.apereira@sp.senac.br>
2022-01-21 13:43:44 -0300

    Adicionando pedido aos usuários

---
+++ src/main/java/com/gusta/SpringApi/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.gusta.SpringApi.services;
+
+import com.gusta.SpringApi.entities.Order;
+import com.gusta.SpringApi.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj =  orderRepository.findById(id);
+        return obj.get();
+    }
+}
