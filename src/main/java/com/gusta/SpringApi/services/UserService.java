9ebf51360fee538f78da6033a3effbb7f63efa4f
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2022-01-25 21:51:49 -0300

    add exceção delete e update

--- src/main/java/com/gusta/SpringApi/services/UserService.java
+++ src/main/java/com/gusta/SpringApi/services/UserService.java
@@ -2,10 +2,14 @@ package com.gusta.SpringApi.services;
 
 import com.gusta.SpringApi.entities.User;
 import com.gusta.SpringApi.repositories.UserRepository;
+import com.gusta.SpringApi.services.exceptions.DatabaseException;
 import com.gusta.SpringApi.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,15 +32,28 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user){
         //O getOne, instancia o usuário mas não pega no banco
         //Ele bascicamente prepara o objeto monitorado
-        User entity = userRepository.getOne(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
