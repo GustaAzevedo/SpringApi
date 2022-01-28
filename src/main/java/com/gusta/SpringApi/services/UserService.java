f87dd0ee21bfc92a6df80b322170743c834baea0
Gustavo de Azevedo Pereira <gustavo.apereira@sp.senac.br>
2022-01-28 15:09:27 -0300

    profieles

--- src/main/java/com/gusta/SpringApi/services/UserService.java
+++ src/main/java/com/gusta/SpringApi/services/UserService.java
@@ -57,8 +57,10 @@ public class UserService {
     }
 
     private void updateData(User entity, User user) {
+        //entity.setId(user.getId());
         entity.setName(user.getName());
         entity.setEmail(user.getEmail());
         entity.setPhone(user.getPhone());
+        //entity.setPassword(user.getPassword());
     }
 }
